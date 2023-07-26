ed6fd562f87de22cad78c08f9836fc3ab8285aad
Michael Glenn <pimpneasily@yahoo.com>
2023-07-26 14:37:48 -0500

    Update CMIP6_for_CORDEX.py

--- CMIP6_for_CORDEX.py
+++ CMIP6_for_CORDEX.py
@@ -13,7 +13,88 @@ bXb5c6gCHESH5PXwPU4jQEE7Ib9J6sbk7ZT2Mw==
 =j+4q
 -----END PGP PUBLIC KEY BLOCK-----
 _
-  ~
+  ~- key: Tok20 Constrained TCR
+  doi: 10.1126/sciadv.aaz9549
+  metric:
+    name: TCR
+    long_name: Transient Climate Response
+    units: K
+    variables: tas
+    comment: >
+      For the TCR values of the individual GCM, we do not use the values proposed initially in Tokarska et al. 2020 but the values provided by th IPCC WGI AR6 on Table 7.SM.5 (https://www.ipcc.ch/report/ar6/wg1/downloads/report/IPCC_AR6_WGI_Chapter_07_Supplementary_Material.pdf), completed by some values from the original article
+  disabled: 
+    cause: preferred_source
+    preferred: Rib21 Constrained TCR
+    comment: More conservative estimate than preferred source
+  type: performance
+  spatial_scope: Global
+  temporal_scope: Annual
+  period:
+    reference: 1981-2014
+    comment: >
+      This is not the period for the metric values (these are model years at
+      the time of doubling CO2), but the one used in the observational constraint.
+  plausible_values:
+    min: 0.9
+    max: 2.27
+    source: reference
+    comment: >
+      Constrained TCR using 1981-2014 temperature past trends, 90% likely range
+      (5-95%) for the TCR. See Table S3. On this Table there are also TCR ranges
+      based on 1981-2017.
+      This period was selected because it leads to a wider, more conservative
+      plausible range.
+  data_source: reference
+  data:
+    ACCESS-CM2_r1i1p1f1: 2.10
+    ACCESS-ESM1-5_r1i1p1f1: 1.95
+    AWI-CM-1-1-MR_r1i1p1f1: 2.06
+    BCC-CSM2-MR_r1i1p1f1: 1.72
+    BCC-ESM1_r1i1p1f1: 1.77
+    CAMS-CSM1-0_r1i1p1f1: 1.73
+    CAS-ESM2-0_r1i1p1f1: 2.04
+    CESM2_r2i1p1f1: 2.06
+    CESM2-FV2_r1i1p1f1: 2.05
+    CESM2-WACCM_r1i1p1f1: 1.98
+    CESM2-WACCM-FV2_r1i1p1f1: 2.01
+    CIESM_r1i1p1f1: 2.39
+    CMCC-CM2-SR5_r1i1p1f1: 2.09
+    CNRM-CM6-1_r1i1p1f2: 2.14
+    CNRM-CM6-1-HR_r1i1p1f2: 2.48
+    CNRM-ESM2-1_r1i1p1f2: 1.86
+    CanESM5_r1-3i1p2f1: 2.74
+    E3SM-1-0_r1i1p1f1: 2.99
+    EC-Earth3_r1i1p1f1: 2.30
+    EC-Earth3-Veg_r1-3i1p1f1: 2.62
+    FGOALS-f3-L_r1i1p1f1: 1.94
+    FGOALS-g3_r1i1p1f1: 1.54
+    FIO-ESM-2-0_r1i1p1f1: 2.22
+    GFDL-ESM4_r1i1p1f1: 1.61 # Missing in AR6 (this is from Ribes et al., 2021)
+    GISS-E2-1-G_r1i1p1f1: 1.80
+    GISS-E2-1-H_r1i1p1f1: 1.93
+    GISS-E2-2-G_r1i1p1f1: 1.71
+    HadGEM3-GC31-LL_r1i1p1f1: 2.55
+    HadGEM3-GC31-MM_r1i1p1f1: 2.58
+    IITM-ESM_r1i1p1f1: 1.71
+    INM-CM4-8_r1i1p1f1: 1.33
+    INM-CM5-0_r1i1p1f1: 1.39 # Missing in AR6 (this is from Ribes et al., 2021)
+    IPSL-CM6A-LR_r1-6i1p1f1: 2.32
+    IPSL-CM6A-LR_r14i1p1f1: 2.32
+    KACE-1-0-G_r1i1p1f1: 1.41
+    MCM-UA-1-0_r1i1p1f1: 1.94
+    MIROC-ES2L_r1i1p1f1: 1.55
+    MIROC6_r1i1p1f1: 1.55
+    MPI-ESM-1-2-HAM_r1i1p1f1: 1.80
+    MPI-ESM1-2-HR_r1i1p1f1: 1.66
+    MPI-ESM1-2-LR_r1i1p1f1: 1.84
+    MRI-ESM2-0_r1i1p1f1: 1.64
+    NESM3_r1i1p1f1: 2.72
+    NorCPM1_r1i1p1f1: 1.56
+    NorESM2-LM_r1i1p1f1: 1.48
+    NorESM2-MM_r1i1p1f1: 1.33
+    SAM0-UNICON_r1i1p1f1: 2.27
+    TaiESM1_r1i1p1f1: 2.34
+    UKESM1-0-LL_r1-3i1p1f2: 2.79
   "$run.json-.js"
 "<.$_-build.py-config"
 ".config.py"
